384dcc03fad4e9d8089719bb92455662de0f6097
nazmul98 <coder.nazmul@gmail.com>
2021-03-07 00:25:17 -0500

    formatted

--- script.js
+++ script.js
@@ -36,23 +36,41 @@ grandTotal.innerText = subTotalAmount + taxAmount;
     });
 });
 
+[...decreamentBtn].forEach((btn, i) => { 
+    btn.addEventListener('click', function () { 
+        let qty = +this.nextElementSibling.value;
+        if (qty > 1) {
+          this.nextElementSibling.value = qty - 1;
+        }
+        this.parentNode.nextElementSibling.innerText =
+            +this.nextElementSibling.value * amounts[i];
+        let subTotalAmount = 0;
+        productsAmount.forEach(amount => {
+            subTotalAmount += +amount.innerHTML;
+        });
+        subTotal.innerText = subTotalAmount;
 
-[...decreamentBtn].forEach((btn, i) => {
-  btn.addEventListener("click", function () {
-    let qty = +this.nextElementSibling.value;
-      if (qty > 1) { 
-        this.nextElementSibling.value = qty - 1;
-      }
-    this.parentNode.nextElementSibling.innerText =
-      +this.nextElementSibling.value * amounts[i];
-    let subTotalAmount = 0;
-    productsAmount.forEach((amount) => {
-      subTotalAmount += +amount.innerHTML;
+        let taxAmount = Math.round(subTotalAmount * 0.1);
+        tax.innerText = taxAmount;
+        grandTotal.innerText = subTotalAmount + taxAmount;
     });
-    subTotal.innerText = subTotalAmount;
-
-    let taxAmount = Math.round(subTotalAmount * 0.1);
-    tax.innerText = taxAmount;
-    grandTotal.innerText = subTotalAmount + taxAmount;
-  });
 });
+// [...decreamentBtn].forEach((btn, i) => {
+//   btn.addEventListener("click", function () {
+//     let qty = +this.nextElementSibling.value;
+//       if (qty > 1) { 
+//         this.nextElementSibling.value = qty - 1;
+//       }
+//     this.parentNode.nextElementSibling.innerText =
+//       +this.nextElementSibling.value * amounts[i];
+//     let subTotalAmount = 0;
+//     productsAmount.forEach((amount) => {
+//       subTotalAmount += +amount.innerHTML;
+//     });
+//     subTotal.innerText = subTotalAmount;
+
+//     let taxAmount = Math.round(subTotalAmount * 0.1);
+//     tax.innerText = taxAmount;
+//     grandTotal.innerText = subTotalAmount + taxAmount;
+//   });
+// });
