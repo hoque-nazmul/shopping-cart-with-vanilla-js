2aeb44b9c2ade87380e2e5ca45cd03d0fc2c5349
nazmul98 <coder.nazmul@gmail.com>
2021-03-07 00:16:32 -0500

    decreament operation done

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ productsAmount.forEach(amount => {
     amounts.push(+amount.innerText);
     subTotalAmount += +amount.innerHTML;
 });
+
 subTotal.innerText = subTotalAmount;
 let taxAmount = Math.round(subTotalAmount * 0.1);
 tax.innerText = taxAmount;
@@ -33,4 +34,25 @@ grandTotal.innerText = subTotalAmount + taxAmount;
         tax.innerText = taxAmount;
         grandTotal.innerText = subTotalAmount + taxAmount;
     });
+});
+
+
+[...decreamentBtn].forEach((btn, i) => {
+  btn.addEventListener("click", function () {
+    let qty = +this.nextElementSibling.value;
+      if (qty > 1) { 
+        this.nextElementSibling.value = qty - 1;
+      }
+    this.parentNode.nextElementSibling.innerText =
+      +this.nextElementSibling.value * amounts[i];
+    let subTotalAmount = 0;
+    productsAmount.forEach((amount) => {
+      subTotalAmount += +amount.innerHTML;
+    });
+    subTotal.innerText = subTotalAmount;
+
+    let taxAmount = Math.round(subTotalAmount * 0.1);
+    tax.innerText = taxAmount;
+    grandTotal.innerText = subTotalAmount + taxAmount;
+  });
 });
