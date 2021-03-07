91290316b6e315fb0792f2508a9a2dca7f25e21c
nazmul98 <coder.nazmul@gmail.com>
2021-03-07 02:46:42 -0500

    organized code with functions

--- script.js
+++ script.js
@@ -4,73 +4,105 @@ const productsAmount = [...document.getElementsByClassName('amount')];
 const subTotal = document.getElementById('sub-total');
 const grandTotal = document.getElementById("grand-total");
 const tax = document.getElementById('tax');
+
 const amounts = [];
-let subTotalAmount = 0;
+// let subTotalAmount = 0;
 
 productsAmount.forEach(amount => {
     amounts.push(+amount.innerText);
-    subTotalAmount += +amount.innerHTML;
+    // subTotalAmount += +amount.innerHTML;
 });
 
-subTotal.innerText = subTotalAmount;
-let taxAmount = Math.round(subTotalAmount * 0.1);
-tax.innerText = taxAmount;
-grandTotal.innerText = subTotalAmount + taxAmount;
 
 
-[...increamentBtn].forEach((btn, i) => { 
-    btn.addEventListener('click', function () { 
-        let qty = +this.previousElementSibling.value;
-        this.previousElementSibling.value = qty + 1;
-        this.parentNode.nextElementSibling.innerText =
-            +this.previousElementSibling.value * amounts[i];
-        let subTotalAmount = 0;
-        productsAmount.forEach(amount => {
-            subTotalAmount += +amount.innerHTML;
-        });
-        subTotal.innerText = subTotalAmount;
+// subTotal.innerText = subTotalAmount;
+// let taxAmount = Math.round(subTotalAmount * 0.1);
+// tax.innerText = taxAmount;
+// grandTotal.innerText = subTotalAmount + taxAmount;
 
-        let taxAmount = Math.round(subTotalAmount * 0.1);
-        tax.innerText = taxAmount;
-        grandTotal.innerText = subTotalAmount + taxAmount;
+// Calculate & Set Value of Total
+const controlTotal = () => { 
+    let subTotalAmount = 0;
+    productsAmount.forEach(amount => {
+      subTotalAmount += +amount.innerHTML;
     });
-});
 
-[...decreamentBtn].forEach((btn, i) => { 
-    btn.addEventListener('click', function () { 
-        let qty = +this.nextElementSibling.value;
-        if (qty > 1) {
-          this.nextElementSibling.value = qty - 1;
-        }
-        this.parentNode.nextElementSibling.innerText =
-            +this.nextElementSibling.value * amounts[i];
-        let subTotalAmount = 0;
-        productsAmount.forEach(amount => {
-            subTotalAmount += +amount.innerHTML;
+    let taxAmount = Math.round(subTotalAmount * 0.1);
+    subTotal.innerText = subTotalAmount;
+    tax.innerText = taxAmount;
+    grandTotal.innerText = subTotalAmount + taxAmount;
+}
+
+controlTotal();
+
+const handleCart = (actionBtn, isIncreament) => { 
+    [...actionBtn].forEach((btn, i) => {
+        btn.addEventListener("click", function () {
+            let qty = +this.parentNode.children[1].value;
+            if (isIncreament) {
+                this.parentNode.children[1].value = qty + 1;
+            } else { 
+                if (qty > 1) {
+                  this.parentNode.children[1].value = qty - 1;
+                }
+            }
+            this.parentNode.nextElementSibling.innerText =
+                +this.parentNode.children[1].value * amounts[i];
+            
+            controlTotal();
+            // let subTotalAmount = 0;
+            // productsAmount.forEach((amount) => {
+            //     subTotalAmount += +amount.innerHTML;
+            // });
+            // subTotal.innerText = subTotalAmount;
+            // let taxAmount = Math.round(subTotalAmount * 0.1);
+            // tax.innerText = taxAmount;
+            // grandTotal.innerText = subTotalAmount + taxAmount;
         });
-        subTotal.innerText = subTotalAmount;
-
-        let taxAmount = Math.round(subTotalAmount * 0.1);
-        tax.innerText = taxAmount;
-        grandTotal.innerText = subTotalAmount + taxAmount;
     });
-});
-// [...decreamentBtn].forEach((btn, i) => {
-//   btn.addEventListener("click", function () {
-//     let qty = +this.nextElementSibling.value;
-//       if (qty > 1) { 
-//         this.nextElementSibling.value = qty - 1;
-//       }
-//     this.parentNode.nextElementSibling.innerText =
-//       +this.nextElementSibling.value * amounts[i];
-//     let subTotalAmount = 0;
-//     productsAmount.forEach((amount) => {
-//       subTotalAmount += +amount.innerHTML;
+}
+
+// For handling Increamental process
+handleCart(increamentBtn, true);
+
+// For handling Decreamental process
+handleCart(decreamentBtn, false);
+
+
+// [...increamentBtn].forEach((btn, i) => { 
+//     btn.addEventListener('click', function () { 
+//         let qty = +this.parentNode.children[1].value;
+//         this.parentNode.children[1].value = qty + 1;
+//         this.parentNode.nextElementSibling.innerText =
+//             +this.parentNode.children[1].value * amounts[i];
+//         let subTotalAmount = 0;
+//         productsAmount.forEach(amount => {
+//             subTotalAmount += +amount.innerHTML;
+//         });
+//         subTotal.innerText = subTotalAmount;
+
+//         let taxAmount = Math.round(subTotalAmount * 0.1);
+//         tax.innerText = taxAmount;
+//         grandTotal.innerText = subTotalAmount + taxAmount;
 //     });
-//     subTotal.innerText = subTotalAmount;
+// });
 
-//     let taxAmount = Math.round(subTotalAmount * 0.1);
-//     tax.innerText = taxAmount;
-//     grandTotal.innerText = subTotalAmount + taxAmount;
-//   });
+// [...decreamentBtn].forEach((btn, i) => { 
+//     btn.addEventListener('click', function () { 
+//         let qty = +this.parentNode.children[1].value;
+//         if (qty > 1) {
+//           this.parentNode.children[1].value = qty - 1;
+//         }
+//         this.parentNode.nextElementSibling.innerText =
+//             +this.parentNode.children[1].value * amounts[i];
+//         let subTotalAmount = 0;
+//         productsAmount.forEach(amount => {
+//             subTotalAmount += +amount.innerHTML;
+//         });
+//         subTotal.innerText = subTotalAmount;
+
+//         let taxAmount = Math.round(subTotalAmount * 0.1);
+//         tax.innerText = taxAmount;
+//         grandTotal.innerText = subTotalAmount + taxAmount;
+//     });
 // });
